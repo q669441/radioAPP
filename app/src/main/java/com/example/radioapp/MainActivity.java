1f2705eede020770fcaee3d314420597f6aadb8f
linda <q669441@gmail.com>
2020-03-30 11:28:45 +0800

    0330第一作業

--- app/src/main/java/com/example/radioapp/MainActivity.java
+++ app/src/main/java/com/example/radioapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.radioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnOK(View view) {
+        TextView textlike =(TextView)findViewById(R.id.textLike);
+        RadioGroup fradioGroup = (RadioGroup) findViewById(R.id.fradioGroup);
+
+        int id =fradioGroup.getCheckedRadioButtonId();
+        RadioButton selected =(RadioButton)findViewById(id);
+textlike.setText("我的最愛是:"+selected.getText());
     }
 }
